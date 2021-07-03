2a3d4e08c2d5a2a7b3ab7a2f66dccd928309ae37
Brandon Duffy <b.duffy.167@gmail.com>
2021-07-03 15:41:56 -0400

    add name properties to transfer in client

--- TenmoClient/Data/Transfer.cs
+++ TenmoClient/Data/Transfer.cs
@@ -13,5 +13,40 @@ namespace TenmoServer.Models
         public int Type { get; set; }
         public int Status { get; set; }
 
+        public string TypeName
+        {
+            get
+            {
+                switch (this.Type)
+                {
+                    case 1000:
+                        return "Request";
+                    case 1001:
+                        return "Send";
+                    default:
+                        return null;
+                }
+            }
+
+        }
+        public string StatusName
+        {
+            get
+            {
+                switch (this.Status)
+                {
+                    case 2000:
+                        return "Pending";
+                    case 2001:
+                        return "Approved";
+                    case 2002:
+                        return "Rejected";
+                    default:
+                        return null;
+                }
+            }
+        }
+
     }
+}
 }
